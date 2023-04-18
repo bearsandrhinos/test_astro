f66bc3038c41360e3e4f1c2e342056ce665dee2b
Peter Whitehead <peterwhitehead01@gmail.com>
2023-04-17 19:35:01 -0700

    new dag

---
+++ dags/test.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime, timedelta 
+
+dag_owner = ''
+
+default_args = {'owner': dag_owner,
+        'depends_on_past': False,
+        'retries': 2,
+        'retry_delay': timedelta(minutes=5)
+        }
+
+with DAG(dag_id='test',
+        default_args=default_args,
+        description='',
+        start_date=datetime(1,1,2023),
+        schedule_interval='None',
+        catchup=False,
+        tags=['']
+):
+
+    start = EmptyOperator(task_id='start')
+
+
+    end = EmptyOperator(task_id='end')
+
+    start  >> end
