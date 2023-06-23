df088f37e1323c8bc0550d93abc1e59c5b9bcf26
Peter Whitehead <peterwhitehead01@gmail.com>
2023-06-23 10:38:38 -0700

    comments and schedule

--- dags/test.py
+++ dags/test.py
@@ -5,6 +5,7 @@ from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQue
 from datetime import datetime, timedelta 
 
 dag_owner = ''
+google_cloud_conn_id = 'google-cloud-default'
 
 default_args = {'owner': dag_owner,
         'depends_on_past': False,
@@ -16,23 +17,26 @@ with DAG(dag_id='test',
         default_args=default_args,
         description='',
         start_date=datetime(2023, 1, 1),
-        schedule_interval='None',
+        schedule_interval='@daily',
         catchup=False,
         tags=['']
 ):
 
     start = EmptyOperator(task_id='start')
 
+#this is just to ensure it actually works
+#another
+
     test_query = BigQueryExecuteQueryOperator (
         task_id='test_query',
         sql = 'SELECT first_name FROM `i-love-basketball.fpl.elements` LIMIT 1',
         destination_dataset_table=None,
         write_disposition='WRITE_EMPTY',
-        gcp_conn_id='google-cloud-default',
+        gcp_conn_id=google_cloud_conn_id,
         use_legacy_sql=False,
         )
 
 
     end = EmptyOperator(task_id='end')
 
-    start  >> test_query >> end
+start  >> test_query >> end
